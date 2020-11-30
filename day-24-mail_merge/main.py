66c1d6cc96e07568f2a2b4315ad063446d99d54b
SpeedyG <speedyg@bitlair.nl>
2020-11-30 13:35:24 +0100

    updated typo and recreated invitations

--- day-24-mail_merge/main.py
+++ day-24-mail_merge/main.py
@@ -1,10 +1,12 @@
+PLACEHOLDER = "[name]"
+
 with open("Input/Names/invited_names.txt", 'r') as name_file:
     names = name_file.readlines()
 
 for name in names:
     with open("Input/Letters/starting_letter.docx") as letter:
         letter_content = letter.read()
-        letter_content = letter_content.replace("[name]", name.strip())
-        invitation = open("Output/ReadyToSend/intivation_for_"+name.strip()+".docx", "w")
+        letter_content = letter_content.replace(PLACEHOLDER, name.strip())
+        invitation = open("Output/ReadyToSend/invitation_for_"+name.strip()+".docx", "w")
         invitation.write(letter_content)
         invitation.close()
