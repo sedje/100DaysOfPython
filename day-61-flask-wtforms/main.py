503474a57a6548dc7ed754407aef862c46b5d9f7
SpeedyG <ankovanderziel@gmail.com>
2021-01-17 16:56:48 +0100

    Completed day 61, WTF Forms

---
+++ day-61-flask-wtforms/main.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import *
+
+
+app = Flask(__name__)
+Bootstrap(app)
+app.secret_key = "6767d20c7eed1f296b636eef765ed46c"
+
+
+class LoginForm(FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField(label="Log In")
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@email.com" and form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template("login.html", form=form)
+
+
+if __name__ == '__main__':
+    app.run()
