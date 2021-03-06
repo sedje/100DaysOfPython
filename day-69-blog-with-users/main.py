4bd078f73488ee2f335db12bc18469735ec5c0ac
SpeedyG <ankovanderziel@gmail.com>
2021-03-06 09:17:01 +0100

    Completed day 69, except for gravatar as I'm not using email as registration form.

--- day-69-blog-with-users/main.py
+++ day-69-blog-with-users/main.py
@@ -11,7 +11,7 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = '8BYkEfBA6O6doNzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
